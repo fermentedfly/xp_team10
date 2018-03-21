4eb98c20234794d687a7641b7b0691c7ab0b81b8
Herbert Fuchs <herbert.fuchs@student.tugraz.at>
2018-03-21 14:47:05 +0100

    [HF,PO] Implemented Login screen with UI Tests

---
+++ app/src/test/java/at/tugraz/xp10/LoginUnitTest.java
@@ -0,0 +1,25 @@
+package at.tugraz.xp10;
+
+import org.junit.Rule;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class LoginUnitTest {
+
+    LoginActivity loginActivity = new LoginActivity();
+    //TODO: ask if tests are necessary
+    @Test
+    public void loginFail() throws Exception{
+
+    }
+    @Test
+    public void loginSuccess() throws Exception {
+
+    }
+    @Test
+    public void pushRegisterButton() throws Exception {
+
+    }
+
+}
