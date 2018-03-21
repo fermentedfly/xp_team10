9986995250d53b2213d0c2e936e700076648b079
Martin.K <martin.k@usr.schoste.com>
2018-03-21 12:01:27 +0100

    [MK,MRS]Fixed testcase added gridview

--- app/src/androidTest/java/at/tugraz/xp10/ListOverviewInstrumentedTest.java
+++ app/src/androidTest/java/at/tugraz/xp10/ListOverviewInstrumentedTest.java
@@ -4,20 +4,25 @@ import android.content.Context;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
+import android.view.View;
+import android.widget.GridView;
 
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertNotNull;
 import static org.junit.Assert.assertNotEquals;
 
 @RunWith(AndroidJUnit4.class)
 public class ListOverviewInstrumentedTest {
 
     @Rule
-    public ActivityTestRule<ListOverview> mActivityRule = new ActivityTestRule<>(
-            ListOverview.class);
+    public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule<>(
+            MainActivity.class);
 
     @Test
     public void useAppContext() throws Exception {
@@ -28,6 +33,6 @@ public class ListOverviewInstrumentedTest {
 
     @Test
     public void test_if_present() throws Exception {
-        assertNotEquals(null, findViewById(R.id.grid_overview));
+        assertNotNull(onView(withText(R.id.grid_overview)));
     }
 }
