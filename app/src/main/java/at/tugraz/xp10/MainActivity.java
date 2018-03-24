191d97b893a8069b9f234e502e3a950cdb8f724d
Ejmi Shkreli <Ejmi@Ejmis-Air.home>
2018-03-25 00:24:35 +0100

    sideBar

--- app/src/main/java/at/tugraz/xp10/MainActivity.java
+++ app/src/main/java/at/tugraz/xp10/MainActivity.java
@@ -80,17 +80,11 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        if  (id == R.id.nav_lists) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_desiredlist) {
 
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_logout) {
 
         }
 
