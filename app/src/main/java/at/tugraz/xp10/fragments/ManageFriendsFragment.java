ac3421d6100f3072740d451be6aaebf129ef74bf
HNikolova <hrisinikolova95@gmail.com>
2018-05-09 09:48:44 +0200

    [MRS, HN] Adding manage friends

---
+++ app/src/main/java/at/tugraz/xp10/fragments/ManageFriendsFragment.java
@@ -0,0 +1,57 @@
+package at.tugraz.xp10.fragments;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import at.tugraz.xp10.R;
+
+public class ManageFriendsFragment extends Fragment {
+    private OnFragmentInteractionListener mListener;
+
+    public ManageFriendsFragment() {
+        // Required empty public constructor
+    }
+
+    public static ManageFriendsFragment newInstance() {
+        ManageFriendsFragment fragment = new ManageFriendsFragment();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_manage_friends, container, false);
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnFragmentInteractionListener) {
+            mListener = (OnFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    public interface OnFragmentInteractionListener {
+        void onFragmentInteraction(Uri uri);
+    }
+}
